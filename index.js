087c3c704c56f8225d7f70c19aa088c36605b320
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-09-08 12:30:25 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+
+    }
+}
+
+const breakfast = new Breakfast("egg", "tea");
+breakfast;
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+const lunch = new Lunch("cabbage", "chicken-soup", "passion-juice");
+lunch;
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+
+const dinner = new Dinner("carrot", "meat", "chicken", "cake");
+dinner;
